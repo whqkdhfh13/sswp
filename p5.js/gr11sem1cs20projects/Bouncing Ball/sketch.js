b11944e54e9678f247d88ab92df5072b800ad7b1
whqkdhfh13 <whqkdhfh13@gmail.com>
2016-09-09 12:09:11 -0600

    Made bouncing ball script

--- p5.js/gr11sem1cs20projects/Bouncing Ball/sketch.js
+++ p5.js/gr11sem1cs20projects/Bouncing Ball/sketch.js
@@ -7,15 +7,37 @@ var ball = {
 
 
 function setup() {
+  
   createCanvas(400,400);
 
 }
 function draw() {
-  background(0);
+  
+    background(0);
+    math(1);
+    display(4);
+    bouncing(0);
+ 
+}
+
+function math(gspeed) {
+ 
+  ball.yspeed +=gspeed;
+  ball.x = ball.x + ball.xspeed;
+  ball.y = ball.y + ball.yspeed;
+
+}
+
+function display(weight) {
+ 
   stroke(225);
-  strokeWeight(4);
+  strokeWeight(weight);
   noFill();
   ellipse(ball.x,ball.y,25,25);
+  
+}
+
+function bouncing(resety) {
   
   if (ball.x > width || ball.x < 0) {
     ball.xspeed = ball.xspeed * -1;
@@ -23,10 +45,9 @@ function draw() {
   
   if (ball.y > height || ball.y < 0) {
     ball.yspeed = ball.yspeed * -1;
+  }  
+
+  if (ball.y > 410) {
+    ball.y = resety;
   }
-  
- 
-  ball.yspeed +=1;
-  ball.x = ball.x + ball.xspeed;
-  ball.y = ball.y + ball.yspeed;
 }
