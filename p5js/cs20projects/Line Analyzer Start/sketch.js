14c032b612e5775da08bbafc4ca4e4b9a3c11c7b
whqkdhfh13 <whqkdhfh13@gmail.com>
2016-09-14 12:09:18 -0600

    Customize Line Analyzer script.

--- p5js/cs20projects/Line Analyzer Start/sketch.js
+++ p5js/cs20projects/Line Analyzer Start/sketch.js
@@ -2,27 +2,31 @@
 
 // Global Variables
 var point1 = {
-	x: -5,
+	x: -7,
 	y: -5,
-	fillColor: "red"
+	fillColor: "red",
+	name: "p1"
 };
 
 var point2 = {
-	x: 5,
+	x: 3,
 	y: 5,
-	fillColor: "blue"
+	fillColor: "blue",
+	name:"p2"
 };
 
 var point3 = {
-	x: 0,
+	x: -2,
 	y: -5,
-	fillColor: "blue"
+	fillColor: "blue",
+	name:"p3"
 };
 
 var point4 = {
-	x: 10,
+	x: 8,
 	y: 5,
-	fillColor: "blue"
+	fillColor: "blue",
+	name:"p4"
 };
 
 var selected = point1; // Use to keep track of which point is currently selected
@@ -36,6 +40,10 @@ function draw() {
 	drawCoordinatePlane();
 	drawLine(point1, point2); // Line 1
 	drawLine(point3, point4); // Line 2
+	textofPoint(point1);
+	textofPoint(point2);
+	textofPoint(point3);
+	textofPoint(point4);
 }
 
 // Events
@@ -46,9 +54,12 @@ function keyTyped() {
 		println("CALCULATION RESULTS");
 
 		// ***** INVOKE YOUR FUNCTIONS BELOW... *****
-		distance(point1,point2)
-
-
+		println("distance p1 to p2="+distance(point1,point2));
+		println("distance p3 to p4="+distance(point3,point4));
+		println("slope p1 to p2="+getslope(point1,point2)[0]+"="+getslope(point1,point2)[2]+"/"+getslope(point1,point2)[1]);
+		println("slope p3 to p4="+getslope(point3,point4)[0]+"="+getslope(point3,point4)[2]+"/"+getslope(point3,point4)[1]);
+		println("midpoint between p1 and p2=("+findmidp(point1,point2)+")");
+		println("midpoint between p3 and p4=("+findmidp(point3,point4)+")");
 
 
 
@@ -99,14 +110,38 @@ function distance(pA, pB) {
 		return d;
 }
 
+function textofPoint(point){
+	var xPix = map(point.x, -10, 10, 0, width);
+	var yPix = map(point.y, -10, 10, height, 0);
+	fill(0,0,255);
+	textSize(15);
+	textWidth(5);
+	text(point.name+"("+point.x+","+point.y+")",xPix+5,yPix-5);
+}
 
+function mousePressed() {
+	if (mouseX > 0 && mouseX < 400 && mouseY > 0 && mouseY < 400) {
+	var mousexpix = map(mouseX, 0, width, -10, 10);
+	var mouseypix = map(mouseY, 0, height, 10, -10);
+	selected.x = round(mousexpix);
+	selected.y = round(mouseypix);
+	}
+}
 
+function getslope(pA, pB) {
+	var run = pB.x - pA.x,
+		rise = pB.y - pA.y,
+		s = rise / run ,
+		sarray = [s,run,rise];
+		return sarray;
+}
 
-
-
-
-
-
+function findmidp(pA, pB) {
+	var midx = (pA.x + pB.x)/2,
+		midy = (pA.y + pB.y)/2,
+		midarray = [midx,midy];
+		return midarray;
+}
 
 
 
