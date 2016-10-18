b9c72701b1bf3e86eb7178ad846a817140de6593
whqkdhfh13 <whqkdhfh13@gmail.com>
2016-10-18 12:09:04 -0600

    Array Drawer script working.

---
+++ js/cs20projects/Array Visualizer/findAverage.js
@@ -0,0 +1,11 @@
+function average(ave, boolearnA) {
+    var hf=0,Ave;
+    for(var i=0; i< ave.length; i++) {
+        hf += ave[i];
+    }
+    Ave = hf / ave.length;
+    if (boolearnA == "on") {
+        println(hf / ave.length);
+    }
+    return Ave;
+}
