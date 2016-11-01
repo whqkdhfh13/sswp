c5da10d6007b25a805f1622ced0ca2b99d0f2429
whqkdhfh13 <whqkdhfh13@gmail.com>
2016-11-01 11:45:13 -0600

    Work 11/1/2016

--- js/cs20projects/Array Visualizer/sketch.js
+++ js/cs20projects/Array Visualizer/sketch.js
@@ -16,9 +16,9 @@ function setup() {
     cnv.parent(container);
 
     // Initialize Variables
-    array1 = [1, 2, 3, 4, 5];
-    array2 = [1, 2, 3, 4, 5];
-    array3 = [1, 2, 3, 4, 5];
+    array1 = [1, 3, 6, 8, 2];
+    array2 = [3, 9, 8, 2, 4];
+    array3 = [4, 1, 3, 8, 7];
 
     //Find the Maximum and Minimum by sort those arrays.
     var fmArray = array1.concat(array2, array3);
@@ -29,8 +29,10 @@ function setup() {
     MaxNum = fmArray[fmArray.length - 1];
     MinNum = fmArray[0];
 
-
-    yMin = 0;
+    yMin = 1;
+    if (yMin > 5) {
+      yMin = MinNum - 5;
+    }
     yMax = 5;
     yScl = 1;
     outputStr = "|| Array Visualizer || / Maximum = " + MaxNum + " / Minimum = " + MinNum + " /";
@@ -47,5 +49,37 @@ function draw() {
 }
 
 function keyPressed() {
+  println(keyCode);
+  if (keyCode == 49) { // #1
+    addNumber(array1, 2);
+  }
+  
+  if (keyCode == 50) { // #2
+    addNumber(array2, 2);
+  }
+  
+  if (keyCode == 51) { // #3
+    addNumber(array3, 2);
+  }
+  
+  if (keyCode == 70) { // F
+    if ((MaxNum - MinNum) / yScl > 15) {
+      yScl +=1;
+    }
+  }
 
+  // Findthe Max and Min everytime I press the key
+  var fmArray = array1.concat(array2, array3);
+  fmArray.sort(function(a, b) {
+    return a - b
+  });
+  MaxNum = fmArray[fmArray.length - 1];
+  MinNum = fmArray[0];
+  outputStr = "|| Array Visualizer || / Maximum = " + MaxNum + " / Minimum = " + MinNum + " /";
+}
+
+function addNumber(array, num) {
+  for (i = 0; i < array.length; i++) {
+      array[i] +=num;
+    }
 }
