df094cbc903431b48e7107860463e1542b1a1beb
whqkdhfh13 <whqkdhfh13@gmail.com>
2016-12-01 12:34:06 -0700

    12/1/2016 Additional Work.

--- js/cs20projects/addRemoveBouncingCirclesStart/bouncingCircle.js
+++ js/cs20projects/addRemoveBouncingCirclesStart/bouncingCircle.js
@@ -1,21 +1,32 @@
-function BouncingCircle(xSpeed, ySpeed) {
+function BouncingCircle(x, y, xSpeed, ySpeed) {
     // Properties (State)
-    this.x = random(width);
-    this.y = random(height);
+    // this.x = random(30, width - 30);
+    // this.y = random(30, height - 30);
+    this.x = x;
+    this.y = y;
     this.r = random(10, 50);
     this.xSpeed = xSpeed;
     this.ySpeed = ySpeed;
     this.col = color(random(255), random(255), random(255));
 
+    this.clicked = function() {
+        var d = dist(mouseX, mouseY, this.x, this.y);
+        if (d <= this.r) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     // Methods (Behaviour)
     this.update = function() {
         // Move & Bounce Horizontally & Vertically
         this.x += this.xSpeed;
-        if (this.x < 0 || this.x > width) {
+        if (this.x <= 0 + 1/2 * this.r || this.x >= width - 1/2 * this.r) {
             this.xSpeed = this.xSpeed * -1;
         }
         this.y += this.ySpeed;
-        if (this.y < 0 || this.y > height) {
+        if (this.y <= 0 + 1/2 * this.r || this.y >= height - 1/2 * this.r) {
             this.ySpeed = this.ySpeed * -1;
         }
     };
@@ -25,5 +36,10 @@ function BouncingCircle(xSpeed, ySpeed) {
         noFill();
         stroke(this.col);
         ellipse(this.x, this.y, 2 * this.r);
+    }
+
+    this.call = function() {
+        this.update();
+        this.display();
     }
 }
--- js/cs20projects/addRemoveBouncingCirclesStart/sketch.js
+++ js/cs20projects/addRemoveBouncingCirclesStart/sketch.js
@@ -1,19 +1,43 @@
 // ADD & REMOVE BOUNCING CIRCLES ASGN START CODE
 
 // DECLARE GLOBAL VARIABLES
-var circle;
+var circles = [];
 
 // SETUP FUNCTION - Runs once at beginning of program
 function setup() {
     createCanvas(600, 400);
 
     // Initialize Variables
-    circle = new BouncingCircle(1, -5);
+    for (var i = 0; i < 20; i++) {
+        circles.push(new BouncingCircle(random(30, width - 30), random(30, height - 30), random(-5, 5), random(-5, 5)));
+    }
 }
 
 // DRAW FUNCTION - Loops @ 60FPS by default
 function draw() {
     background(0);
-    circle.update();
-    circle.display();
+    for (var i = 0; i < circles.length; i++) {
+        circles[i].call();
+    }
+}
+
+function mousePressed() {
+    var clicked = false;
+    for (var i = 0; i < circles.length; i++) {
+        if (circles[i].clicked()) {
+            circles.splice(i, 1);
+            clicked = true;
+            break;
+        }
+    }
+
+    if (!clicked) {
+        circles.push(new BouncingCircle(mouseX, mouseY, random(-5, 5), random(-5, 5)));
+    }
+}
+
+function keyPressed() {
+    if (keyCode == 32) {
+        circles = [];
+    }
 }
