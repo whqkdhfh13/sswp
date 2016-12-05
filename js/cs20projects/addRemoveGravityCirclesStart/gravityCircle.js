52265c0d20dd9b316a4a371b09261a28d790adc4
whqkdhfh13 <whqkdhfh13@gmail.com>
2016-12-05 12:10:19 -0700

    12/5/2016 Work School.

--- js/cs20projects/addRemoveGravityCirclesStart/gravityCircle.js
+++ js/cs20projects/addRemoveGravityCirclesStart/gravityCircle.js
@@ -1,30 +1,44 @@
-function GravityCircle() {
+function GC(x, y) {
     // Properties (State)
-    this.x = random(width);
-    this.y = random(height);
+    // this.x = random(width);
+    // this.y = random(height - 1/2 * this.r);
     this.r = random(25, 50);
+    this.x = x;
+    this.y = y;
     this.xSpeed = random(-5, 5);
     this.ySpeed = 0;
     this.g = random(0.25, 0.75);
     this.launchSpeed = random(-10, -30);
     this.col = color(random(255), random(255), random(255));
 
+    this.clicked = function() {
+        var d = dist(mouseX, mouseY, this.x, this.y);
+        if (d <= this.r) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
     // Methods (State)
     this.update = function() {
-        // Move & Bounce this object Horizontally
-        this.x += this.xSpeed;
-        if (this.x < 0 || this.x > width) {
-            this.xSpeed = this.xSpeed * -1;
-        }
-
         // Move & Bounce this object Vertically
         this.y += this.ySpeed;
         this.ySpeed += this.g;  // Apply gravity
-        if (this.y > height) {
-            this.y = height; // Set to ground position
+        if (this.y > height - 1/2 * this.r) {
+            this.y = height - 1/2 * this.r; // Set to ground position
             this.ySpeed = this.launchSpeed; // Reset to launch speed
             this.launchSpeed = this.launchSpeed * 0.95; // Energy loss
         }
+
+        this.x += this.xSpeed;
+        if (this.x <= 0 + 1/2 * this.r || this.x >= width - 1/2 * this.r ) {
+            this.xSpeed = this.xSpeed * -1;
+        }
+        if (this.launchSpeed > -5) {
+            this.xSpeed *= 0.99;
+        }
+
     };
 
     this.display = function() {
@@ -32,5 +46,10 @@ function GravityCircle() {
         stroke(this.col);
         strokeWeight(4);
         ellipse(this.x, this.y, 2 * this.r);
+    };
+
+    this.call = function() {
+        this.update();
+        this.display();
     };
 }
--- js/cs20projects/addRemoveGravityCirclesStart/sketch.js
+++ js/cs20projects/addRemoveGravityCirclesStart/sketch.js
@@ -1,20 +1,41 @@
 // ADD & REMOVE GRAVITY CIRCLES ASGN START CODE
 
 // DECLARE GLOBAL VARIABLES
-var circle;
+var circles = [];
 
 // SETUP FUNCTION - Runs once at beginning of program
 function setup() {
     createCanvas(600, 400);
 
     // Initialize Global Variables
-    circle = new GravityCircle();
+    for (var i = 0; i < 20; i++) {
+        circles.push(new GC(random(25, width - 25), random(0, height - 25)));
+    }
 }
 
 // DRAW FUNCTION - Loops @ 60FPS by default
 function draw() {
     background(0);
-    circle.update();
-    circle.display();
+    for (var i = 0; i < circles.length; i++) {
+        circles[i].call();
 
+        if (circles[i].launchSpeed > -1) {
+            circles.splice(i, 1);
+        }
+    }
+}
+
+function mousePressed() {
+    var clicked = false;
+    for (var i = 0; i < circles.length; i++) {
+        if (circles[i].clicked()) {
+            circles.splice(i, 1);
+            clicked = true;
+            break;
+        }
+    }
+
+    if (!clicked) {
+        circles.push(new GC(mouseX, mouseY));
+    }
 }
