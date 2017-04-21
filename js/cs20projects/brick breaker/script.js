2b5a05242b739d98cae1568fe1937d426c48222f
whqkdhfh13 <whqkdhfh13@gmail.com>
2017-04-21 12:08:32 -0600

    4/21/2017 Work School.

--- js/cs20projects/brick breaker/script.js
+++ js/cs20projects/brick breaker/script.js
@@ -1,17 +1,34 @@
-var username;
+var id = [];
+var password = [];
+var nickname = [];
+var authArray = [];
 $(document).ready(function() {
 
     $("canvas").hide();
 
-    $("#a").click(function() {
-        // Hello User
-        username = prompt("What is your name?");
-        console.log(username);
-        if (username !== "" || username == " ") {
-            alert("Hello " + username + "!");
-            document.getElementById("name").innerHTML = "Good luck on your quiz, " + username + "!";
-        } else {
-            alert("You didn't type anything!");
-        }
+    $("#singup").click(function() {
+        id = localStorage.id.split(";");
+        password = localStorage.password.split(";");
+        nickname = localStorage.nickname.split(";");
+
     });
+
 });
+
+function authorizations(input, type) {
+    var authType;
+    if (type == id) {
+        authType = $("#inputId").val();
+    } else {
+        authType = $("#inputPw").val();
+    }
+
+    var tempStr;
+    tempStr = localStorage.getItem(input);
+    authArray = tempStr.split(";");
+
+    for (var i = 0; i < authArray.length; i++) {
+        
+    }
+
+}
