5ada5dd319c7962d31e9a214d6fb08ad01961a02
whqkdhfh13 <whqkdhfh13@gmail.com>
2017-05-02 12:09:19 -0600

    5/2/2017 Work School / Add signup script.

--- js/cs20projects/brick breaker/script.js
+++ js/cs20projects/brick breaker/script.js
@@ -1,13 +1,7 @@
 var id = [];
 var password = [];
 var nickname = [];
-var authArray = [];
 $(document).ready(function() {
-
-    // Load id, password and nickname.
-    id = localStorage.id.split(";");
-    password = localStorage.password.split(";");
-    nickname = localStorage.nickname.split(";");
 
     $("canvas").hide();
 
@@ -17,29 +11,49 @@ $(document).ready(function() {
     });
 
     $("#singup").click(function() {
+        loadData();
+        var tempUsername = prompt("Type id that you want to use.");
+        if (authorizations(tempUsername, "id")) {
+            alert("ID that you typed are already exist. \n Please try again.");
+        } else if (tempUsername == "") {
 
+        } else {
+            var tempNickname = prompt("Type nickname that you want to use.");
+            if (authorizations(tempNickname, "nickname")) {
+                alert("Nickname that you typed are already exist.\n Please try again.");
+            } else {
+                var tempPassword = prompt("Type password that you want to use.");
+
+                // Push datas to localStorage.
+                id.push(tempUsername);
+                localStorage.id = id.join(";");
+                nickname.push(tempNickname);
+                localStorage.nickname = nickname.join(";");
+                password.push(tempPassword);
+                localStorage.password = password.join(";");
+            }
+
+        }
     });
 
 });
 
 function authorizations(input, type) {
-    var authType, tempStr;
-
-    if (type == id) {
-        authType = $("#inputId").val();
-        tempStr = localStorage.getItem(id);
-    } else {
-        authType = $("#inputPw").val();
-        tempStr = localStorage.getItem(password);
-    }
+    var authType = input, tempStr = localStorage.getItem(type), authArray;
     authArray = tempStr.split(";");
 
     for (var i = 0; i < authArray.length; i++) {
         if (authArray[i] == authType) {
-            return false;
-        } else {
             return true;
+        } else {
+            return false;
         }
     }
 
+}
+
+function loadData() {
+    id = localStorage.id.split(";");
+    password = localStorage.password.split(";");
+    nickname = localStorage.nickname.split(";");
 }
