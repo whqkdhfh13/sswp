cc7fb2622f67d09d454b943cafdea4c4ac14ba13
whqkdhfh13 <whqkdhfh13@gmail.com>
2017-05-04 12:09:27 -0600

    5/4/2017 Work School / Sign up error fixed. Add checkspace function.

--- js/cs20projects/brick breaker/script.js
+++ js/cs20projects/brick breaker/script.js
@@ -14,23 +14,28 @@ $(document).ready(function() {
         loadData();
         var tempUsername = prompt("Type id that you want to use.");
         if (authorizations(tempUsername, "id")) {
-            alert("ID that you typed are already exist. \n Please try again.");
-        } else if (tempUsername == "") {
+            alert("ID that you typed is already exist. \nPlease try again.");
+        } else if (tempUsername == "" || tempUsername == undefined) {
 
         } else {
             var tempNickname = prompt("Type nickname that you want to use.");
             if (authorizations(tempNickname, "nickname")) {
-                alert("Nickname that you typed are already exist.\n Please try again.");
+                alert("Nickname that you typed is already exist.\nPlease try again.");
+            } else if (tempNickname == "" || tempNickname == undefined) {
+
             } else {
                 var tempPassword = prompt("Type password that you want to use.");
+                if (tempPassword !== "") {
+                    alert("Your signup was successfully done!");
 
-                // Push datas to localStorage.
-                id.push(tempUsername);
-                localStorage.id = id.join(";");
-                nickname.push(tempNickname);
-                localStorage.nickname = nickname.join(";");
-                password.push(tempPassword);
-                localStorage.password = password.join(";");
+                    // Push datas to localStorage.
+                    id.push(tempUsername);
+                    localStorage.id = id.join(";");
+                    nickname.push(tempNickname);
+                    localStorage.nickname = nickname.join(";");
+                    password.push(tempPassword);
+                    localStorage.password = password.join(";");
+                }
             }
 
         }
@@ -39,20 +44,35 @@ $(document).ready(function() {
 });
 
 function authorizations(input, type) {
-    var authType = input, tempStr = localStorage.getItem(type), authArray;
-    authArray = tempStr.split(";");
-
+    var tempStr = localStorage.getItem(type);
+    var authArray = tempStr.split(";");
     for (var i = 0; i < authArray.length; i++) {
-        if (authArray[i] == authType) {
+        if (authArray[i] == input) {
             return true;
-        } else {
-            return false;
         }
     }
+    return false;
+}
 
+function spaceCheck(input) {
+    var tempArray = input.split("");
+    var checksum = 0;
+    for (var i = 0; i < tempArray.length; i++) {
+        if (tempArray[i] == " ") {checksum++;}
+    }
+    if (checksum == tempArray.length) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function loadData() {
+    if (localStorage.id == undefined || localStorage.id == "") {
+        localStorage.id = " ";
+        localStorage.nickname = " ";
+        localStorage.password = " ";
+    }
     id = localStorage.id.split(";");
     password = localStorage.password.split(";");
     nickname = localStorage.nickname.split(";");
