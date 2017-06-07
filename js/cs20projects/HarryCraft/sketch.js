fca5f9977cefd976b2a2fb0be9dd7c39cf8fc082
whqkdhfh13 <whqkdhfh13@gmail.com>
2017-06-07 12:10:03 -0600

    6/7/2017 Work School / Error detected in render, for loop

--- js/cs20projects/HarryCraft/sketch.js
+++ js/cs20projects/HarryCraft/sketch.js
@@ -16,7 +16,8 @@ function setup() {
 }
 
 function draw() {
-    background(0,0,0,50);
+    background(0, 0, 0 );
+    // world[3][0][0] = 1;
     if (cons === 1) {
         info();
     }
@@ -24,33 +25,35 @@ function draw() {
         player.ax = 0;
         player.ay = 0;
     } else {
-      rotation();
+        rotation();
     }
-    fill(0,255,255,50);
-    rect(width/2 - width/20, height/2 - height/20, width/10, height/10);
-	  // I want this circle to be displayed between 300 and 60.
+    fill(0, 255, 255, 50);
+    rect(width / 2 - width / 20, height / 2 - height / 20, width / 10, height / 10);
+    // I want this circle to be displayed between 300 and 60.
     fill(255);
-	  ellipse  ((width*(1-(player.ax+60)/120)), (height*(player.ay+60)/120), 50);
-
+    ellipse((width * (1 - (player.ax + 60) / 120)), (height * (player.ay + 60) / 120), 50);
+    render();
 }
 
 
 function createWorld() {
     // Make the world
-    var tWorld = [], tLevel = [], tArray = [];
+    var tWorld = [],
+        tLevel = [],
+        tArray = [];
 
     // Make basic array that contains number of i
-    for (var i = 0; i < 100; i++) {
+    for (var i = 0; i < 10; i++) {
         tArray.push("0");
     }
 
     // Push tArray to tLevel 100 times
-    for (var i = 0; i < 100; i++) {
+    for (var i = 0; i < 10; i++) {
         tLevel.push(tArray);
     }
 
     // Push tLevel to tWorld 100 times
-    for (var level = 0; level < 100; level++) {
+    for (var level = 0; level < 10; level++) {
         tWorld.push(tLevel);
     }
 
@@ -58,14 +61,17 @@ function createWorld() {
     return tWorld;
 }
 
-function lender() {
+function render() {
     for (var i = 0; i < world.length; i++) {
         for (var j = 0; j < world[i].length; j++) {
             for (var k = 0; k < world[i][j].length; k++) {
-                if(world[i][j][k] == 1) {
-                    var xpos = sin(90/extendedAngle*(atan(i/j) - player.ax) + 45);
-                    var ypos = sin(90/extendedAngle*(atan(k/j) - player.ay) + 45);
-
+                if (world[i][j][k] == 1) {
+                    if ((abs(atan(i/j)-player.ax) < 75 && abs(atan(k/j)-player.ay) < 75) || calc3Dist(player.x, player.y, player.z, i, j, k) < 5) {
+                        var xPos = map(tan(atan(i/j)-player.ax), -tan(60), tan(60), 0, width);
+                        var yPos = map(tan(atan(k/j)-player.ay), -tan(60), tan(60), 0, height);
+                        var rad = pow((1/2), calc3Dist(player.x, player.y, player.z, i, j, k)) + 0.03;
+                        ellipse(xPos, yPos, rad * width, rad * height);
+                    }
                 }
             }
         }
@@ -75,28 +81,28 @@ function lender() {
 function player() {
     this.x = 0;
     this.y = 0;
+    this.z = 0;
     this.ax = 0;
     this.ay = 0;
     this.speed = 0;
-
 }
 
 function rotation() {
-	// Make sure to make if statement for squaring that have + and -.
+    // Make sure to make if statement for squaring that have + and -.
     if (!(mouseX > width * 0.45 && mouseX < width * 0.55)) {
-        if (mouseX-width/2 > 0) {
-				player.ax += sq(2*(mouseX - width/2) / (width/2));
-			} else if (mouseX-width/2 < 0) {
-				player.ax -= sq(2*(mouseX - width/2) / (width/2));
-			}
+        if (mouseX - width / 2 > 0) {
+            player.ax += sq(2 * (mouseX - width / 2) / (width / 2));
+        } else if (mouseX - width / 2 < 0) {
+            player.ax -= sq(2 * (mouseX - width / 2) / (width / 2));
+        }
     }
 
     if (!(mouseY > height * 0.45 && mouseY < height * 0.55)) {
-			if (mouseY-height/2 > 0) {
-				player.ay -= sq(1.5*(mouseY - height/2) / (height/2));
-			} else if (mouseY-height/2 < 0) {
-				player.ay += sq(1.5*(mouseY - height/2) / (height/2));
-			}
+        if (mouseY - height / 2 > 0) {
+            player.ay -= sq(1.5 * (mouseY - height / 2) / (height / 2));
+        } else if (mouseY - height / 2 < 0) {
+            player.ay += sq(1.5 * (mouseY - height / 2) / (height / 2));
+        }
     }
 
     if (player.ax < -180) {
@@ -105,36 +111,35 @@ function rotation() {
         player.ax -= 360;
     }
 
-		if (player.ay > 90) {
-			player.ay = 90;
-		} else if (player.ay < -90) {
-			player.ay = -90;
-
-		}
+    if (player.ay > 90) {
+        player.ay = 90;
+    } else if (player.ay < -90) {
+        player.ay = -90;
+    }
 }
 
 function info() {
     stroke(255);
     fill(255);
     textSize(18);
-    text("ax = "+player.ax, 5, 18);
-    text("ay = "+player.ay, 5, 36);
+    text("ax = " + player.ax, 5, 18);
+    text("ay = " + player.ay, 5, 36);
 }
 
 function centerCanvas() {
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / 2;
-  cnv.position(x, y);
+    var x = (windowWidth - width) / 2;
+    var y = (windowHeight - height) / 2;
+    cnv.position(x, y);
 }
 
 function windowResized() {
-  centerCanvas();
+    centerCanvas();
 }
 
 function keyPressed() {
     console.log(keyCode);
 }
 
-function calc3Dist(x1,y1,z1,x2,y2,z2) {
+function calc3Dist(x1, y1, z1, x2, y2, z2) {
     return sqrt(sq(x2 - x1) + sq(y2 - y1) + sq(z2 - z1));
 }
