cf34d7bdfd1a301690337cb825f1550cc13d0b97
whqkdhfh13 <whqkdhfh13@gmail.com>
2018-02-07 15:32:50 -0700

    2/7/2018 Work School - Happy Birthday!

--- java/helloworld/src/helloworld/Ball.java
+++ java/helloworld/src/helloworld/Ball.java
@@ -6,16 +6,30 @@ public class Ball {
 	private float xSpeed = 2;
 	private float ySpeed = 2;
 	
-	void constructor(float x, float y, float xSpeed, float ySpeed) {
+	public Ball(float x, float y, float xSpeed, float ySpeed) {
 		this.x = x;
 		this.y = y;
 		this.xSpeed = xSpeed;
 		this.ySpeed = ySpeed;
 	}
 	
-	void move() {
+	public void move() {
 		this.x += this.xSpeed;
 		this.y += this.ySpeed;
+	}
+	
+	public float[] getPos() {
+		float[] temp = {x, y};
+		return temp;
+	}
+	
+	public String getPos(int t) {	
+		return "x : " + this.x + " | y : " + this.y;
+	}
+	
+	public static void main(String[] args) {
+		Ball abc = new Ball(3,4,2,2);
+		abc.getPos();
 	}
 }
 
