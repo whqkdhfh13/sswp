519e2369158d9b3a81844e0f06017a5a4507765f
whqkdhfh13 <whqkdhfh13@gmail.com>
2018-05-29 03:00:53 -0600

    Home

--- java/RandomStuff/src/randomstuff/NewJFrame.java
+++ java/RandomStuff/src/randomstuff/NewJFrame.java
@@ -1,38 +1,34 @@
-//﻿/*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
 package randomstuff;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
-import static randomstuff.RandomStuff.*;
+import javax.swing.UnsupportedLookAndFeelException;
 
-/**
- *
- * @author j.cho18
- */
 public class NewJFrame extends javax.swing.JFrame {
     
-//    static boolean isPaused = true;
+    // Initialize boolean variables and pause object
     static boolean isFinished = false;
     static boolean isStarted = false;
-    static final Object PAUSELCK = new Object();
+    static boolean isPaused = false;
     static boolean swt = false;
+    static final Object PAUSELCK = new Object();
 
-    /**
-     * Creates new form NewJFrame
-     */
+    // Initialize Swing GUI's own variables
     public NewJFrame() {
         initComponents();
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="This is my own functions">
+    
+    public static void pl(Object a) {
+        System.out.println(a);
     }
     
     public static List<Integer> FindP(int j, Object... n) {
         List<Integer> temp = new ArrayList<>();
         for (int i = 0; i < j; i++) {
-            if (isPalindromeWithOnlyInt(i)) temp.add(i);
+            if (RandomStuff.isPalindromeWithOnlyInt(i)) temp.add(i);
         }
         if ((boolean)arrCheck(0, n)[0])
                 if ((boolean)n[(int)arrCheck(0, n)[1]])
@@ -70,20 +66,16 @@ public class NewJFrame extends javax.swing.JFrame {
             temp[i] = tNums.toArray();
         }
         return (Object[])temp[a];
-    }
+    } // </editor-fold>
     
-    public static Object msT(double howMany, Command aFunc, int n, Object... p) throws InterruptedException {
+    public static Object msT(double howMany, RandomStuff.Command aFunc, int n, Object... p) throws InterruptedException {
         float a = 0;
         boolean isTrue = false;
         
         if (howMany < 1) {
             howMany = 1;
         }
-        
-//        if (jButton1.getText() == "Start") {
-//            jButton1.setText("Restart");
-//        }
-        
+                
         if ((boolean)arrCheck(0, p)[0]) {
             for (int j = 1; j < arrCheck(0, p).length; j++) {
                 if ((boolean)p[(int)arrCheck(0, p)[j]]) isTrue = true;                
@@ -106,17 +98,11 @@ public class NewJFrame extends javax.swing.JFrame {
         }  
                
         for (double i = 1; i < howMany + 1; i++) {
-                        
-            if (swt) {
-                i = 1;
-                a = 0;
-                swt = !swt;
-            }
-            
-            if (pb.isSelected()) {
+                                    
+            if (isPaused) {
                 if (!isStarted) {
-                    abc.setText("Total elapsed time");
-                    abd.setText("Average elapsed time");
+                    abc.setText("Total elapsed time will be shown here...");
+                    abd.setText("Average elapsed time will be shown here...");
                     abe.setText("Waiting for user's input ...");
                     acc.setValue(0);
                     acc.setString("0%");
@@ -128,7 +114,8 @@ public class NewJFrame extends javax.swing.JFrame {
                 }
                 catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
-                }                
+                }
+                isPaused = false;
             } 
             if (!isStarted) {
                 a = 0;
@@ -144,7 +131,7 @@ public class NewJFrame extends javax.swing.JFrame {
             if (isTrue) {
                 abc.setText("Total elapsed time = "+String.format("%.5f", a/1e6f)+"ms");
                 abd.setText("Average elapsed time = "+String.format("%.5f", a/(1e6f * i))+"ms");
-                abe.setText("Progress - " + (int)i + " / " + (int)howMany);
+                abe.setText("In Progress - " + (int)i + " / " + (int)howMany);
                 acc.setValue((int) (10000*i/howMany));
                 acc.setString(String.format("%.2f", 100 * i / howMany) + "%");
             }           
@@ -156,14 +143,18 @@ public class NewJFrame extends javax.swing.JFrame {
                 }
                 try {
                     synchronized (PAUSELCK) {
-                        PAUSELCK.wait();
+                        PAUSELCK.wait();                        
                     }
                 }
                 catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                 }
-            }          
-            
+            }      
+            if (swt) {
+                i = 1;
+                a = 0;
+                swt = !swt;
+            }            
         }
         
         if ((boolean)arrCheck(1, p)[0]) {
@@ -180,35 +171,22 @@ public class NewJFrame extends javax.swing.JFrame {
         return a / (howMany * (float)p[(int)arrCheck(4, p)[1]]);
     }
 
-    /**
-     * This method is called from within the constructor to initialize the form.
-     * WARNING: Do NOT modify this code. The content of this method is always
-     * regenerated by the Form Editor.
-     */
-    @SuppressWarnings("unchecked")
+
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        acc = new javax.swing.JProgressBar();
         abe = new javax.swing.JLabel();
         abc = new javax.swing.JLabel();
+        acc = new javax.swing.JProgressBar();
         abd = new javax.swing.JLabel();
         jCheckBox1 = new javax.swing.JCheckBox();
-        pb = new javax.swing.JToggleButton();
         sb = new javax.swing.JButton();
+        pb = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(153, 255, 255));
         setBounds(new java.awt.Rectangle(1, 1, 30, 30));
         setFocusable(false);
-
-        acc.setBackground(new java.awt.Color(204, 0, 0));
-        acc.setForeground(new java.awt.Color(255, 153, 0));
-        acc.setMaximum(10000);
-        acc.setBorder(null);
-        acc.setBorderPainted(false);
-        acc.setString("");
-        acc.setStringPainted(true);
 
         abe.setText("Waiting for user's input ...");
 
@@ -218,23 +196,31 @@ public class NewJFrame extends javax.swing.JFrame {
         abc.setToolTipText("");
         abc.setVerticalAlignment(javax.swing.SwingConstants.TOP);
         abc.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        abc.setMaximumSize(new java.awt.Dimension(300, 16));
+        abc.setMaximumSize(new java.awt.Dimension(210, 16));
+        abc.setMinimumSize(new java.awt.Dimension(180, 16));
+        abc.setPreferredSize(new java.awt.Dimension(210, 16));
+
+        acc.setBackground(new java.awt.Color(15, 191, 186));
+        acc.setForeground(new java.awt.Color(255, 153, 0));
+        acc.setMaximum(10000);
+        acc.setBorder(null);
+        acc.setBorderPainted(false);
+        acc.setRequestFocusEnabled(false);
+        acc.setString("...");
+        acc.setStringPainted(true);
+        acc.setVerifyInputWhenFocusTarget(false);
 
         abd.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         abd.setText("Average elapsed time");
         abd.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        abd.setMaximumSize(new java.awt.Dimension(210, 16));
+        abd.setMinimumSize(new java.awt.Dimension(180, 16));
+        abd.setPreferredSize(new java.awt.Dimension(210, 16));
 
         jCheckBox1.setText("Close when done");
         jCheckBox1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jCheckBox1ActionPerformed(evt);
-            }
-        });
-
-        pb.setText("Pause");
-        pb.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                pbActionPerformed(evt);
             }
         });
 
@@ -245,73 +231,67 @@ public class NewJFrame extends javax.swing.JFrame {
             }
         });
 
+        pb.setForeground(new java.awt.Color(0, 0, 0));
+        pb.setText("Pause");
+        pb.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                pbActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(abd, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(abc, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(acc, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(2, 2, 2)
-                        .addComponent(jCheckBox1)
                         .addGap(18, 18, 18)
-                        .addComponent(pb)
-                        .addGap(6, 6, 6)
-                        .addComponent(sb))
+                        .addComponent(abe, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(abd, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(abc, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(acc, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addGap(12, 12, 12)
-                        .addComponent(abe)))
-                .addContainerGap(38, Short.MAX_VALUE))
+                        .addGap(29, 29, 29)
+                        .addComponent(jCheckBox1)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(pb, javax.swing.GroupLayout.PREFERRED_SIZE, 190, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(sb, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(12, 12, 12)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(acc, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(2, 2, 2)
-                        .addComponent(abe)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                    .addComponent(abe))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(abc, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(abd)
-                .addGap(13, 13, 13)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(abd, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jCheckBox1))
+                .addGap(11, 11, 11)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(pb)
-                        .addComponent(jCheckBox1))
-                    .addComponent(sb))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(pb, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(sb, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void pbActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pbActionPerformed
-        // TODO add your handling code here:
-        if(isStarted) {
-            if(!isFinished) {
-                synchronized(PAUSELCK) {
-                    PAUSELCK.notifyAll();
-                }
-            }
-            sb.setText("Resume");
-        }
-    }//GEN-LAST:event_pbActionPerformed
-
+    // <editor-fold defaultstate="collapsed" desc="ActionEvents"> 
     private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
         // TODO add your handling code here:
         if (isFinished && jCheckBox1.isSelected()) {
             System.exit(0);
         }
     }//GEN-LAST:event_jCheckBox1ActionPerformed
-
+       
     private void sbActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sbActionPerformed
         // TODO add your handling code here:
         pl("つO.0っ");
@@ -319,34 +299,33 @@ public class NewJFrame extends javax.swing.JFrame {
             sb.setText("Restart");
         }
 
-        if (pb.isSelected()) {
-            pb.doClick();
-            sb.setText("Restart");
-        } 
-        if (isStarted){
+        if (isStarted && !isPaused){
+            pl("WHAT?");
             swt = true;
-            pb.doClick();
-            sb.setText("Start");
+            isPaused = true;
             isStarted = false;
-        }
-        synchronized(PAUSELCK) {
-            PAUSELCK.notifyAll();
+            isFinished = false;
+            sb.setText("Start");
         }
         
-//        Thread.currentThread().interrupt();
-//        initComponents();
-//        try {
-//            main(new String[] {"restart"});
-//        } catch (InterruptedException | InvocationTargetException ex) {
-//            Logger.getLogger(NewJFrame.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+        if (isStarted && isPaused) {
+            isPaused = false;
+            sb.setText("Restart");
+        } 
+        synchronized(PAUSELCK) {
+            PAUSELCK.notifyAll();
+        }        
     }//GEN-LAST:event_sbActionPerformed
 
-    /**
-     * @param args the command line arguments
-     * @throws java.lang.InterruptedException
-     * @throws java.lang.reflect.InvocationTargetException
-     */
+    private void pbActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pbActionPerformed
+        // TODO add your handling code here:
+        if(isStarted && !isFinished) {
+            isPaused = true;
+            sb.setText("Resume");
+        }
+    }//GEN-LAST:event_pbActionPerformed
+    // </editor-fold>
+    
     public static void main(String... args) throws InterruptedException, InvocationTargetException {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -360,13 +339,7 @@ public class NewJFrame extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
@@ -383,14 +356,16 @@ public class NewJFrame extends javax.swing.JFrame {
         }
         
     }
-
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private static javax.swing.JLabel abc;
     private static javax.swing.JLabel abd;
     private static javax.swing.JLabel abe;
     private static javax.swing.JProgressBar acc;
     private static javax.swing.JCheckBox jCheckBox1;
-    private static javax.swing.JToggleButton pb;
+    private static javax.swing.JButton pb;
     private static javax.swing.JButton sb;
     // End of variables declaration//GEN-END:variables
 }
+
+
