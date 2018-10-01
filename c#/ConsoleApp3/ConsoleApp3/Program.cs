fe39d7e1d1985a9b4996c86936846d03c7fc6095
Jayden Junhyeon Cho <j.cho18@share.epsb.ca>
2018-10-01 11:08:36 -0600

    School

--- c#/ConsoleApp3/ConsoleApp3/Program.cs
+++ c#/ConsoleApp3/ConsoleApp3/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
 
 
 namespace ConsoleApp3 {
@@ -12,7 +14,7 @@ namespace ConsoleApp3 {
         readonly static private string[] sObject = new string[] { "Apple", "Microsoft", "school", "the god", "the monitor" };
         static Random rnd = new Random();
 
-        private static string[] makeInsults(string[] sN, string[] sV, string[] sO, int howMany) {
+        private static string[] MakeInsults(string[] sN, string[] sV, string[] sO, int howMany) {
             string[] result = new string[howMany];
 
             for (int i = 0; i < howMany; i++) {
@@ -23,14 +25,91 @@ namespace ConsoleApp3 {
 
         }
 
-        static void Main (string[] args) {
-            string[] temp = makeInsults(sName, sVerb, sObject, 10);
+        private static void SaveInsults(string inputFileName, string[] inputArray, bool putLine) {
+            string fileName;
 
-            foreach (string sTemp in temp) {
-                Console.WriteLine(sTemp);
+            if (!inputFileName.Contains(".txt"))
+                fileName = inputFileName + ".txt";
+            else
+                fileName = inputFileName;
+
+            StreamWriter sw = new StreamWriter(fileName); 
+
+            for(int i = 0; i < inputArray.Length; i++) {
+                sw.WriteLine((i+1) + ". " + inputArray[i]);
             }
-            Console.ReadLine();
+            sw.Close();
 
+            Console.WriteLine(System.AppDomain.CurrentDomain.BaseDirectory + fileName);
+        }
+
+        private static void SaveInsults (string inputFileName, string[] inputArray) {
+            string fileName;
+            string currentPath = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            if (!inputFileName.Contains(".txt"))
+                fileName = inputFileName + ".txt";
+            else
+                fileName = inputFileName;
+
+            StreamWriter sw = new StreamWriter(fileName);
+
+            foreach (string sTemp in inputArray) {
+                sw.WriteLine(sTemp);
+            }
+            sw.Close();
+
+            Console.WriteLine("\n" + currentPath + fileName);
+            string executeCmd = "/C \"" + System.AppDomain.CurrentDomain.BaseDirectory + fileName + "\"\nexit";
+            System.Diagnostics.Process.Start("CMD.exe", executeCmd);
+        }
+
+        private static void ICA27() {
+            string[] aTemp;
+            while (true) {
+                Console.Write("!! Insult Generator !!\n\nPlease submit the number of insults that you want to make: ");
+                try {
+                    uint sNum = uint.Parse(Console.ReadLine());
+                    aTemp = MakeInsults(sName, sVerb, sObject, (int)sNum);
+                    Console.WriteLine("\n// List of insults\n");
+                    break;
+                }
+                catch (Exception e) {
+                    Console.Clear();
+                    Console.WriteLine("Wrong input. Please try again with the integer number greater than 0.\n\n" + e + "\n");
+                }
+            }
+
+            for (int i = 0; i < aTemp.Length; i++) {
+                Console.WriteLine((i + 1) + ". " + aTemp[i]);
+            }
+            while (true) {
+                try {
+                    Console.WriteLine("\nIf you want to save this in a new file, please type the name of the file. If not, Please press Enter.");
+                    string sTemp = Console.ReadLine();
+
+                    if (sTemp.Length > 0) {
+                        SaveInsults(sTemp, aTemp);
+                        Console.WriteLine("\nPlease press Enter to exit...");
+                        Console.ReadLine();
+                    }
+                    break;
+                }
+                catch (Exception e) {
+                    Console.WriteLine("Wrong File Name. Please avoid any special characters.\n\n" + e + "\n");
+                }
+            }
+        }
+
+        static void Main (string[] args) {
+            Console.Write("1. Insult Generator\n\nPlease select the program that you want to execute: ");
+            int temp = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            if (temp == 1) {
+                ICA27();
+            }
         }
     }
 }
