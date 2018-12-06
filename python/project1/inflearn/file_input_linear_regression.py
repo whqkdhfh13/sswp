1f817736aa5d47befc64845367146edde9585016
Jayden Junhyeon Cho <j.cho18@share.epsb.ca>
2018-12-06 10:49:09 -0700

    School

--- python/project1/inflearn/file_input_linear_regression.py
+++ python/project1/inflearn/file_input_linear_regression.py
@@ -12,7 +12,7 @@ dataLength = 6
 
 print("Successfully loaded data.\nx: " + str(xData.shape) + " | y: " + str(yData.shape) if (len(xData) == dataLength and len(yData) == dataLength) else "Error")
 
-x = tf.placeholder(tf.float32, shape=[3, 1])
+x = tf.placeholder(tf.float32, shape=[None, 3])
 y = tf.placeholder(tf.float32, shape=[None, 1])
 w = tf.Variable(tf.random_normal([3, 1]), name='weight')
 b = tf.Variable(tf.random_normal([1]), name='bias')
@@ -21,4 +21,34 @@ hypothesis = tf.matmul(x, w) + b
 
 cost = tf.reduce_mean(tf.square(hypothesis - y))
 
-optimizer = tf.train.Ada
+aOptimizer = tf.train.AdamOptimizer(learning_rate = 1)
+aTrain = aOptimizer.minimize(cost)
+
+gOptimizer = tf.train.GradientDescentOptimizer(learning_rate = 2e-5)
+gTrain = gOptimizer.minimize(cost)
+
+sess = tf.Session()
+
+sess.run(tf.global_variables_initializer())
+
+for step in range(20001):
+	if step == 0:
+		cost_val = 1
+
+	cost_val, hy_val, _ = sess.run(
+		[cost, hypothesis, aTrain if cost_val > 6 else gTrain],
+		feed_dict = {x: xData, y: yData}
+	)
+
+	# if step % 100 == 0:
+	# 	print(step, "Cost: ", cost_val, "\nPrediction:\n", hy_val)
+
+	if step == 20000:
+		for temp in hy_val:
+			print(temp, "±", np.sqrt(cost_val))
+		s = 0
+		for i in range(len(yData)):
+			s += (yData[i] - hy_val[i]) / len(hy_val)
+		print(s * len(hy_val), " | ", s)
+
+		print("\nYour score will be ", sess.run(hypothesis, feed_dict = {x: [[90, 93, 96]]}))
