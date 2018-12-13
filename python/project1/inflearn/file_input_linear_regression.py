7fcf40cbb330585a323c019ff656b2d23baffdcd
Jayden Junhyeon Cho <j.cho18@share.epsb.ca>
2018-12-13 10:47:17 -0700

    School

--- python/project1/inflearn/file_input_linear_regression.py
+++ python/project1/inflearn/file_input_linear_regression.py
@@ -10,11 +10,11 @@ xData = xy[:, 0:-1]
 yData = xy[:, [-1]]
 dataLength = 6
 
-print("Successfully loaded data.\nx: " + str(xData.shape) + " | y: " + str(yData.shape) if (len(xData) == dataLength and len(yData) == dataLength) else "Error")
+print(("Successfully loaded data.\nx: " + str(xData.shape) + " | y: " + str(yData.shape)) if (len(xData) == dataLength and len(yData) == dataLength) else "Error")
 
-x = tf.placeholder(tf.float32, shape=[None, 3])
-y = tf.placeholder(tf.float32, shape=[None, 1])
-w = tf.Variable(tf.random_normal([3, 1]), name='weight')
+x = tf.placeholder(tf.float32, shape=[xData.shape[0], xData.shape[1]])
+y = tf.placeholder(tf.float32, shape=[yData.shape[0], yData.shape[1]])
+w = tf.Variable(tf.random_normal([xData.shape[1], yData.shape[1]]), name='weight')
 b = tf.Variable(tf.random_normal([1]), name='bias')
 
 hypothesis = tf.matmul(x, w) + b
@@ -27,15 +27,18 @@ sess = tf.Session()
 
 sess.run(tf.global_variables_initializer())
 
+# CONTINUE FUNCTIONALIZE THE PROCESS
 
 def printresult(cost_value, original_result, actual_result, session, test_value):
+
+	x = tf.placeholder(tf.float32, shape = [len(test_value), len(test_value[0])])
 	s = 0
 	for i in range(len(original_result)):
 		s += (original_result[i] - actual_result[i])
 	print("/Most Approximate values/\n")
 	for temp in actual_result:
-		sh = temp - 2 * s
-		sl = temp + 2 * s
+		sh = temp - 2 * (s + np.sqrt(cost_value))
+		sl = temp + 2 * (s + np.sqrt(cost_value))
 		if sh > sl:
 			print(temp, "|", sl, "-", sh)
 		else:
@@ -50,44 +53,46 @@ def printresult(cost_value, original_result, actual_result, session, test_value)
 f = 0
 c = 0
 d = True
+t = 0
+
+
+def training(x_data, y_data):
+	print("hi")
+
 
 for step in range(30001):
-	s = 0
 
 	if step == 0:
 		cost_val = 1
-		s = 1
 
-	if c % 1000 == 0 and c == 1000:
-		aOptimizer = tf.train.GradientDescentOptimizer(learning_rate = 1e-5)
+	if c == 1000:
+		aOptimizer = tf.train.GradientDescentOptimizer(learning_rate = 2e-5)
 		aTrain = aOptimizer.minimize(cost)
 		print("hi |", c, "|", d, "|", f - cost_val, "|",)
-		c += 1
+
+	if t == 1000:
+		printresult(cost_val, yData, hy_val, sess, [[50., 50., 50.], [60., 60., 60.], [40., 60., 80.]])
+		break
 
 	cost_val, hy_val, _ = sess.run(
 		[cost, hypothesis, aTrain],
 		feed_dict = {x: xData, y: yData}
 	)
 
-	if abs(f - cost_val) < 1e-5 and d is True:
+	if abs(f - cost_val) < cost_val / 1e-3 and d is True:
 		d = False
 		print("Hello", abs(f - cost_val))
 
-	if abs(f - cost_val) < 1e-5 / 1e-4 and d is False:
+	if abs(f - cost_val) < 1e-7 and abs(f - cost_val) != 0 and d is False:
 		c += 1
+
+	if abs(f - cost_val) == 0:
+		t += 1
 
 	f = cost_val
 
-	if step % 3000 == 0:
+	if step % 1000 == 0:
 		print(step, "Cost: ", cost_val, "\nPrediction:\n", hy_val)
 
-	if step == 30000:
-		printresult(cost_val, yData, hy_val, sess, [[50, 50, 50], [60, 60, 60], [40, 60, 80]])
-		# for temp in hy_val:
-		# 	print(temp, "±", 2 * np.sqrt(cost_val))
-		# s = 0
-		# for i in range(len(yData)):
-		# 	s += (yData[i] - hy_val[i]) / len(hy_val)
-		# print(s * len(hy_val), " | ", s)
-		#
-		# print("\nYour score will be ", sess.run(hypothesis, feed_dict = {x: [[90, 93, 96]]}))
+else:
+	printresult(cost_val, yData, hy_val, sess, [[50, 50, 50], [60, 60, 60], [40, 60, 80]])
