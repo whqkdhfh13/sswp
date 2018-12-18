0e3e39a4fbdc61491e269308b9ee653b37e2addf
Jayden Junhyeon Cho <j.cho18@share.epsb.ca>
2018-12-18 11:12:31 -0700

    School

--- python/project1/inflearn/file_input_linear_regression.py
+++ python/project1/inflearn/file_input_linear_regression.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import os
 import numpy as np
+import sys
 
 # Turning off Tensorflow warning message in program output
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -8,31 +9,23 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 xy = np.loadtxt('data_01.txt', delimiter = ',', dtype = np.float32)
 xData = xy[:, 0:-1]
 yData = xy[:, [-1]]
-dataLength = 6
 
 print(
-	("Successfully loaded data.\nx: " + str(xData.shape) + " | y: " + str(yData.shape)) if (len(xData) == dataLength and len(yData) == dataLength) else "Error")
-
+	("Successfully loaded data.\nx: " + str(xData.shape) + " | y: " + str(yData.shape)) if xData.shape[0] == yData.shape[0] else "Error")
 
 # CONTINUE FUNCTIONALIZE THE PROCESS
 
-def initializer(x_data, y_data):
-	global x_holder, y_holder, w_holder, b_holder
+
+def training(x_data, y_data, temp_sess = None, degree = 1, run_count = 30000, cdv = 1e-3, test_value = [[]]):
+	x_data = np.array(x_data)
+	y_data = np.array(y_data)
 	x_holder = tf.placeholder(tf.float32, shape = [None, x_data.shape[1]])
 	y_holder = tf.placeholder(tf.float32, shape = [None, y_data.shape[1]])
 	w_holder = tf.Variable(tf.random_normal([x_data.shape[1], y_data.shape[1]]), name = 'temp_weight')
 	b_holder = tf.Variable(tf.random_normal([1]), name = 'temp_bias')
 
-# ERROR WHEN TEMP_HYPOTHESIS IS DEFINED BY AN USER / FEEDING SHAPE ERROR
-def training(x_data, y_data, temp_sess = None, temp_hypothesis = None, run_count = 30001, cdv = 1e-3, test_value = [[]]):
-	x_holder = tf.placeholder(tf.float32, shape = [None, x_data.shape[1]])
-	y_holder = tf.placeholder(tf.float32, shape = [None, y_data.shape[1]])
-	w_holder = tf.Variable(tf.random_normal([x_data.shape[1], y_data.shape[1]]), name = 'temp_weight')
-	b_holder = tf.Variable(tf.random_normal([1]), name = 'temp_bias')
-
-	if temp_hypothesis is None:
-		temp_hypothesis = tf.matmul(x_holder, w_holder) + b_holder
-	temp_cost = tf.reduce_mean(tf.square(temp_hypothesis - y_holder))
+	hypothesis = tf.matmul(x_holder ** degree, w_holder) + b_holder
+	temp_cost = tf.reduce_mean(tf.square(hypothesis - y_holder))
 
 	temp_optimizer = tf.train.AdamOptimizer(learning_rate = .01, epsilon = 1e-12)
 	temp_train = temp_optimizer.minimize(temp_cost)
@@ -48,7 +41,6 @@ def training(x_data, y_data, temp_sess = None, temp_hypothesis = None, run_count
 
 	# Defining function to give clarity to the code
 	def printresult(cost_value, original_result, actual_result):
-		feed_value = np.array(test_value)
 		s = 0
 		for i in range(len(original_result)):
 			s += (original_result[i] - actual_result[i])
@@ -62,33 +54,41 @@ def training(x_data, y_data, temp_sess = None, temp_hypothesis = None, run_count
 				print(temp, "|", sh, "-", sl)
 		print("\nCost =", cost_value, "=", np.sqrt(cost_value), "^ 2\ns =", s)
 
-		if feed_value.shape[0] > 0:
-			print(temp_hypothesis.eval(session = temp_sess, feed_dict = {x_holder: feed_value}))
+		feed_value = np.array(test_value)
+
+		if feed_value.shape[1] != x_data.shape[1]:
+			print("InvalidTestDataValue - Size of the test data does not match the trained data. Please check it again.")
+			sys.exit()
+
+		if feed_value.shape[0] > 0 and feed_value.shape[1] > 0:
+			print(hypothesis.eval(session = temp_sess, feed_dict = {x_holder: feed_value}))
 
 		temp_sess.close()
+
 	# End of the defined function
 
 	# Start the training phase
-	for temp_step in range(run_count):
-		temp_cost_value, temp_hypothesis_value, _ = temp_sess.run([temp_cost, temp_hypothesis, temp_train], feed_dict = {x_holder: x_data, y_holder: y_data})
+	for temp_step in range(run_count + 1):
+		temp_cost_value, temp_hypothesis_value, _ = temp_sess.run([temp_cost, hypothesis, temp_train], feed_dict = {x_holder: x_data, y_holder: y_data})
 
 		temp_difference = abs(temp_final - temp_cost_value)
 
-		if temp_count == 1000:
+		if temp_count == 1000 and temp_endCount == 0:
 			temp_optimizer = tf.train.GradientDescentOptimizer(learning_rate = 2e-5)
 			temp_train = temp_optimizer.minimize(temp_cost)
+			temp_count += 1
 
-		if temp_endCount == 1000:
+		if temp_endCount == 1000 or temp_step == run_count:
 			return printresult(temp_cost_value, y_data, temp_hypothesis_value)
 
 		if temp_difference < temp_cost_value / cdv and temp_triggered is False:
 			temp_triggered = True
 			print("Hello", temp_difference)
 
-		if temp_difference < 1e-7 and temp_difference != 0 and temp_triggered is True:
+		if temp_difference < 1e-7 and temp_difference != 0 and temp_triggered is True and temp_count < 1000:
 			temp_count += 1
 
-		if temp_difference == 0:
+		if temp_difference == 0 and temp_count == 1001:
 			temp_endCount += 1
 
 		temp_final = temp_cost_value
@@ -97,5 +97,4 @@ def training(x_data, y_data, temp_sess = None, temp_hypothesis = None, run_count
 			print(temp_step, "Cost: ", temp_cost_value, "\nPrediction:\n", temp_hypothesis_value)
 
 
-initializer(xData, yData)
-training(xData, yData, test_value = [[50, 50, 50]])
+training(xData, yData, test_value = [[40, 40, 40, 40], [60, 70, 80, 70]])
