79663a491e378926b43b561bac94f857ac781cd4
Jayden Junhyeon Cho <j.cho18@share.epsb.ca>
2019-03-20 12:26:38 -0600

    School

---
+++ java/progContest/src/lowestPossibleCombination.java
@@ -0,0 +1,24 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// @author j.cho18
+public class lowestPossibleCombination {
+    public static void main(String[] args) throws Exception {	
+        int lines=1;
+
+        //<editor-fold defaultstate="collapsed" desc=" Generated Code ">
+	String line = "";
+	
+	java.util.Scanner in= new java.util.Scanner(System.in);
+	while (lines > 0 && in.hasNextLine()) {
+	    line += in.nextLine();
+	    lines--;
+	}
+        //</editor-fold>
+	
+        System.out.println(line);
+    }
+}
